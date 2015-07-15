4a591322b599a5dc5627b49b1dbd4f318e78bc8a
Jan Boon <janboon438@gmail.com>
2015-07-15 07:47:27 +0200

    First shot at processing the province borders

---
+++ src/com/mylab/ProvinceBorders.java
@@ -0,0 +1,105 @@
+package com.mylab;
+
+import javax.xml.parsers.*;
+import org.xml.sax.*;
+import org.xml.sax.helpers.*;
+import java.util.*;
+import java.io.*;
+
+public class ProvinceBorders {
+
+	public static void main(String argv[]) {
+		try {
+
+			SAXParserFactory factory = SAXParserFactory.newInstance();
+			SAXParser saxParser = factory.newSAXParser();
+
+			DefaultHandler handler = new DefaultHandler() {
+
+				boolean bMultiGeometry = false;
+				boolean bPolygon = false;
+				boolean bouterBoundaryIs = false;
+				boolean bLinearRing = false;
+				boolean bcoordinates = false;
+
+				public void startElement(String uri, String localName,
+						String qName, Attributes attributes)
+						throws SAXException {
+
+					System.out.println("Start Element :" + qName);
+
+					if (qName.equalsIgnoreCase("MultiGeometry")) {
+						bMultiGeometry = true;
+					}
+					
+					if (qName.equalsIgnoreCase("Polygon")) {
+						bPolygon = true;
+					}
+
+					if (qName.equalsIgnoreCase("outerBoundaryIs")) {
+						bouterBoundaryIs = true;
+					}
+
+					if (qName.equalsIgnoreCase("LinearRing")) {
+						bLinearRing = true;
+					}
+
+					if (qName.equalsIgnoreCase("coordinates")) {
+						bcoordinates = true;
+					}
+
+				}
+
+				public void endElement(String uri, String localName,
+						String qName) throws SAXException {
+
+					System.out.println("End Element :" + qName);
+
+				}
+
+				public void characters(char ch[], int start, int length)
+						throws SAXException {
+
+					if (bMultiGeometry) {
+						System.out.println("MultiGeometry : "
+								+ new String(ch, start, length));
+						bMultiGeometry = false;
+					}
+
+					if (bPolygon) {
+						System.out.println("Polygon : "
+								+ new String(ch, start, length));
+						bPolygon = false;
+					}
+
+					if (bouterBoundaryIs) {
+						System.out.println("bouterBoundaryIs : "
+								+ new String(ch, start, length));
+						bouterBoundaryIs = false;
+					}
+
+					if (bLinearRing) {
+						System.out.println("LinearRing : "
+								+ new String(ch, start, length));
+						bLinearRing = false;
+					}
+
+					if (bcoordinates) {
+						System.out.println("coordinates : "
+								+ new String(ch, start, length));
+						bcoordinates = false;
+					}
+
+				}
+
+			};
+
+			saxParser.parse("/home/jan/Downloads/geoserver-GetFeature.kml", handler);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
